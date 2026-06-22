ca37cee822c0ce19a838b08e81d89b7e66316712
elie-jerdak <jardakelie@gmail.com>
2026-06-22 22:02:11 +0300

    Testing Swagger API

--- FitnessClub.Api/Program.cs
+++ FitnessClub.Api/Program.cs
@@ -166,11 +166,11 @@ using (var scope = app.Services.CreateScope())
 app.UseRateLimiter();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 

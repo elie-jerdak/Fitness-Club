cb78162c255952db543c25aa1156c909a7f414fc
elie-jerdak <jardakelie@gmail.com>
2026-07-12 11:16:01 +0300

    Testing Heatmap

--- FitnessClub.Api/Program.cs
+++ FitnessClub.Api/Program.cs
@@ -12,7 +12,6 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Stripe;
 using System;
@@ -162,7 +161,7 @@ app.UseRateLimiter();
     app.UseSwaggerUI();
 //}
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); 
 
 app.UseCors("AllowFrontend");
 

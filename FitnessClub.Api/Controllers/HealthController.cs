0dde4a2fa65de466f5e00312bd36158d7dd27110
elie-jerdak <jardakelie@gmail.com>
2026-07-12 12:26:46 +0300

    Added Health Endpoint that wakes api service before sending credentials from cms

---
+++ FitnessClub.Api/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace FitnessClub_Test.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Console.WriteLine("Health Endpoint hit successfully");
+            return Ok(new
+            {
+                status = "running",
+                timestamp = DateTime.UtcNow
+            });
+        }
+    }
+}
